bbb79a8e1ec3721edfcd072cc9d8beaffbd9a101
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-12-17 23:25:18 -0500

    Update ethersjsLib.js

--- ethersjsLib.js
+++ ethersjsLib.js
@@ -2,10 +2,27 @@ const {ethers} = require('ethers');
 const rpcURL = 'https://cloudfare-eth.com/';
 const provider = new ethers.providers.JsonRpcProvider(rpcURL);
 
-const address = '0xx73BCEb1Cd57C711feaC4224D062b0F6ff338501e';
+const address = '0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e';
 
 const main = async () => {
     const balance = await provider.getBalance(address);
     console.log(`\nETH Balance of ${address} --> ${ethers.utils.formatEth}`);
 }
 main();
+
+////////////////////////////////
+//Second part reading the Smart Contract//
+const {ethers} = require('ethers');
+const rpcURL = 'https://cloudfare-eth.com/';
+const provider = new ethers.providers.JsonRpcProvider(rpcURL);
+
+const ERC20_ABI = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function totalSupply() view returns (uint256)",
+    "function balanceOf(address) view returns (uint)",
+    ];
+
+const address = '0x6B73CEb1Cd57C711aFbC4624D062b0F6ff3985f73';
+const contract = new ethers.Contract(address, ERC20_ABI, provider);
+
